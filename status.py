edaf0e626e5c8f4f920f04ff0650df211f6ef5fe
witam3 <szymekcos@wp.pl>
2025-07-20 15:11:26 +0200

    init commit

---
+++ status.py
@@ -0,0 +1,21 @@
+class Status:
+    def __init__(self):
+        self.spd = 0
+        self.ea = 0
+        self.pa = 0
+        self.ed = 0
+        self.pd = 0
+        self.acc = 0
+        self.paralyze = 0
+        self.shield = 0
+        self.paralyze_immunity = 0
+        self.sleep = 0
+        self.sleep_immunity = 0
+        self.confuse = 0
+        self.confuse_immunity = 0
+        self.anti_heal = 0
+        self.anti_heal_immunity = 0
+# do sprawdzenia, rozne trutki, rozne dot
+#trucizna 10 i 10 sie nie stackuja ale 10 i 14 tak
+# bleed i switch nigdy sie nie stackuja
+#czy switch sie dodaje?
